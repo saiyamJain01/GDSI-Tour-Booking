6d544124bb49c32a254f2c9cad69d5d8b372e365
saiyamJain01 <saiyam.25bai10938@vitbhopal.ac.in>
2026-09-16 15:09:35 +0530

    Fix code in constructor parameter

--- TourPackage.java
+++ TourPackage.java
@@ -8,16 +8,17 @@ public class TourPackage {
     
     public TourPackage(String destination, int duration, double basePrice) {
             this.destination = destination;  // THIS KEYWORD LEFT SIDE IS VARIABLE NAME RIGHT SIDE IS CONSTRUCTOR PARAMETER
-            this.duration = durations;
+            this.duration = duration;
             this.basePrice = basePrice;
     }
 
-    // Getter method: Destination ka naam read karne ke liye
+    // Getter/Setter
     public String getDestination() {
         return destination;  // RETURN TYPE METHOD
     }
 
-    // Package details print karne ka method
+
+
     public void showDetails() {
         System.out.println(" Destination: " + destination + " | Days: " + duration + " |  Price: Rs " + basePrice);
     }
