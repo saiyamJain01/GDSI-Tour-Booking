c3038419bb6dfa95b599e06ab2c20869f3975759
saiyamJain01 <saiyam.25bai10938@vitbhopal.ac.in>
2026-09-16 14:32:49 +0530

    Refactor TourPackage class for variable consistency
    Updated variable names and fixed constructor parameter to enhance code consistency.

--- TourPackage.java
+++ TourPackage.java
@@ -1,23 +1,24 @@
 public class TourPackage {
-    // Encapsulation: Variables ko private rakha hai taki bahar se direct change na ho
+    // ACCESS MODIFIERS , PUBLIC CAN BE ACCESS FROM ANYWHERE
     private String destination;
-    private int durationDays;
+    private int duration;
     private double basePrice;
 
-    // Constructor: Jab naya package banega tab ye run hoga
-    public TourPackage(String destination, int durationDays, double basePrice) {
-        this.destination = destination;
-        this.durationDays = durationDays;
-        this.basePrice = basePrice;
+
+    
+    public TourPackage(String destination, int duration, double basePrice) {
+            this.destination = destination;  // THIS KEYWORD LEFT SIDE IS VARIABLE NAME RIGHT SIDE IS CONSTRUCTOR PARAMETER
+            this.duration = durations;
+            this.basePrice = basePrice;
     }
 
     // Getter method: Destination ka naam read karne ke liye
     public String getDestination() {
-        return destination;
+        return destination;  // RETURN TYPE METHOD
     }
 
     // Package details print karne ka method
     public void showDetails() {
-        System.out.println(" Destination: " + destination + " | Days: " + durationDays + " |  Price: Rs " + basePrice);
+        System.out.println(" Destination: " + destination + " | Days: " + duration + " |  Price: Rs " + basePrice);
     }
 }
